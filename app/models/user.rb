e9c11d58666db2271245aa22c271be1c254022eb
tankwanghow <tankwanghow@gmail.com>
2012-07-14 16:44:35 +0800

    more user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  attr_accessible :username, :password, :password_confirmation
+  attr_accessible :username, :name, :password, :password_confirmation
   validates :password, confirmation: true, on: :create
   validates :password, on: :create, presence: true
   validates :username, uniqueness: true
-  validates    :name, :username, presence: true
+  validates :name, :username, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,15 +6,28 @@ describe User do
   it { should validate_confirmation_of :password }
   it { create :user; should validate_uniqueness_of :username }
 
-  it "should validate_presence_of :password on: create" do
-    build(:user, password: nil, password_confirmation: nil).should have(1).error_on(:password)
+  it "should protect attributes :salt, :status and :crypted_password" do
+    user = create :user, status: 'active', salt: 'haha', crypted_password: 'mama'
+    expect(user.status).not_to be('active')
+    expect(user.salt).not_to be('haha')
+    expect(user.crypted_password).not_to be('mama')
   end
 
-  it "should not validate_presence_of :password on: update" do
-    user = create :user
-    user.password = nil
-    user.password_confirmation = nil
-    user.should have(:no).error_on :password
+  it "should validate_presence_of :password on :create" do
+    expect(build :user, password: nil, password_confirmation: nil ).to have(1).error_on(:password)
+  end
+
+  it "should not validate_presence_of :password on :update" do
+    create :user, username: 'bob'
+    user = User.find_by_username 'bob'
+    expect(user.password).to be_nil
+    expect(user).to have(:no).error_on :password
+  end
+
+  it "should encrypt password" do
+    user = build :user, password: 'secret', password_confirmation: 'secret'
+    user.save!
+    expect(user.crypted_password).not_to be('secret')
   end
 
 end
