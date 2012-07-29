a9a195bbba985a1aec02ef6cba7f781f21976aac
tankwanghow <tankwanghow@gmail.com>
2012-07-30 00:36:10 +0800

    user model should not be audited

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
   include SentientUser
-  #include ActiveModel::ForbiddenAttributesProtection
 
   attr_accessible :username, :name, :password, :password_confirmation
   validates :password, confirmation: true, on: :create
   validates :password, on: :create, presence: true
   validates :username, uniqueness: true
   validates :name, :username, presence: true
-
-  simple_audit username_method: :username do |r|
-    {
-      username: r.username,
-      name: r.name,
-      status: r.status
-    }
-  end
 
   include AASM
   aasm column: :status do
