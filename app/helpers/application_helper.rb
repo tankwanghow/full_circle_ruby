682391e4e6b2e1374846496ab5384a8451a27d85
tankwanghow <tankwanghow@gmail.com>
2012-08-04 02:35:30 +0800

    created main nav bars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def menu text, &block
+    a_text = text.html_safe + content_tag(:b, nil, class: 'caret')
+    child = block_given? ? capture(&block) : nil
+    content_tag :li, class: 'dropdown' do
+      content_tag(:a, a_text, class: 'dropdown-toggle', href: '#', data: { toggle: 'dropdown'}) +
+      content_tag(:ul, child, class: 'dropdown-menu')
+    end
+  end
+
+  def menu_child text, url
+    content_tag :li do
+      link_to text, url
+    end
+  end
+
 end
