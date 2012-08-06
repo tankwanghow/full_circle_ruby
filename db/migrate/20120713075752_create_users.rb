fc64f3d27e8dc530b080215e3936a46667942c4c
tankwanghow <tankwanghow@gmail.com>
2012-08-06 17:19:57 +0800

    added is_admin col

--- db/migrate/20120713075752_create_users.rb
+++ db/migrate/20120713075752_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string     :username,        null: false
       t.string     :name
       t.string     :password_digest, null: false
+      t.boolean    :is_admin,        default: false
       t.string     :status,          default: 'pending'
       t.integer    :lock_version,    default: 0
       t.timestamps
