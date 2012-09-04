d28babd6d169a50b64bcce9f9be2df3301be2519
tankwanghow <tankwanghow@gmail.com>
2012-09-05 04:17:40 +0800

    display in default date format

--- app/inputs/datepicker_input.rb
+++ app/inputs/datepicker_input.rb
@@ -1,7 +1,7 @@
 class DatepickerInput < SimpleForm::Inputs::Base
   
   def input
-    @builder.text_field attribute_name, input_html_options    
+    @builder.text_field attribute_name, input_html_options.merge(value: @builder.object.send(attribute_name))
   end
   
 end
