3d4f81c367c49a4b9a476bc748f9b86db65e3ed5
tankwanghow <tankwanghow@gmail.com>
2012-09-12 23:24:47 +0800

    move search_params to lib, added global rescue_exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,52 +1,31 @@
 class ApplicationController < ActionController::Base
   include Authentication
   include SentientController
+  include StoreSearchParamsSession
 
   protect_from_forgery
   before_filter :login_required
-  before_filter :save_search_params_to_session
 
   rescue_from Exception do |e|
     msg = "#{e.class.name}"
     msg += "<br/>#{e.message}"
-    flash.now[:error] = msg.html_safe
-    redirect_to :back 
-  end 
-
-private
-
-  def search_term
-    params[:search] ? params[:search][:terms] : session[:search_term]
+    flash[:error] = msg.html_safe
+    redirect_to exception_redirection_path
   end
 
-  def search_date_from
-    params[:search] ? params[:search][:date_from] : session[:search_date_form]
-  end
+  private
 
-  def search_date_to
-    params[:search] ? params[:search][:date_to] : session[:search_date_to]
-  end
-
-  def search_amount_larger
-    params[:search] ? params[:search][:amount_larger] : session[:search_amount_larger]
-  end
-
-  def search_amount_less
-    params[:search] ? params[:search][:amount_less] : session[:search_amount_less]
-  end
-
-  def save_search_params_to_session
-    drop_blank_search_params
-    session[:search_term] = search_term
-    session[:search_date_form] = search_date_from
-    session[:search_date_to] = search_date_to
-    session[:search_amount_larger] = search_amount_larger
-    session[:search_amount_less] = search_amount_less
-  end
-
-  def drop_blank_search_params
-    if params[:search]
-      params[:search].delete_if { |k,v| v.blank? }
+  def exception_redirection_path
+    if params
+      if params[:action] == 'update'
+        {controller: params[:controller], id: params[:id], action: 'edit'}
+      elsif params[:action] == 'create'
+        {controller: params[:controller], action: 'new'}
+      else
+        :back
+      end
+    else
+      :back
     end
   end
 
