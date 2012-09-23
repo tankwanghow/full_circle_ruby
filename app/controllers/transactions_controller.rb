51061b148fd72db16917a106ab829631ecb8fb3a
tankwanghow <tankwanghow@gmail.com>
2012-09-23 20:57:15 +0800

    create session[:transaction_query] first

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,11 +1,11 @@
 class TransactionsController < ApplicationController
   def index
-    session[:transactions_query_start_date] ||= Date.current - 30
-    session[:transactions_query_end_date] ||= Date.current
-    store_param :transactions_query 
-    account = Account.find_by_name1(session[:transactions_query_name])
+    store_param :transactions_query
+    session[:transactions_query][:start_date] ||= Date.current - 30
+    session[:transactions_query][:end_date] ||= Date.current
+    account = Account.find_by_name1(session[:transactions_query][:name])
     if account
-      @transactions = account.statement(session[:transactions_query_start_date], session[:transactions_query_end_date])
+      @transactions = account.statement(session[:transactions_query][:start_date], session[:transactions_query][:end_date])
     else
       @transactions = []
     end
