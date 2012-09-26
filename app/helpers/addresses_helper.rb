c28e8678e995086f49718df328e6e76c07dce826
tankwanghow <tankwanghow@gmail.com>
2012-09-26 17:32:50 +0800

    added pdf format for displaying address

--- app/helpers/addresses_helper.rb
+++ app/helpers/addresses_helper.rb
@@ -10,5 +10,20 @@ module AddressesHelper
       address.email
     ].map! { |t| !t.blank? ? sanitize(t) : nil }.compact.join("<br/>").html_safe
   end
+
+  def payment_header_address_pdf address
+    [address.address1, address.address2, address.address3,
+     address.zipcode, address.city, address.state, address.country].map! do |t|
+      !t.blank? ? sanitize(t) : nil
+    end.compact.join(", ")
+  end
+
+  def payment_header_contact_pdf address
+    tel = address.tel_no.blank? ? nil : "Tel No: #{address.tel_no}"
+    fax = address.fax_no.blank? ? nil : "Fax No: #{address.fax_no}"
+    em  = address.email.blank?  ? nil : "Email: #{address.email}"
+    [tel, fax, em].compact.join(", ")
+  end
+
 end
 
