f11a79841003970b834899993e5c251c2dc31a0c
tankwanghow <tankwanghow@gmail.com>
2012-09-30 20:39:48 +0800

    dont init CompanyAddress if Addresses table is not migrated

--- config/initializers/full_circle.rb
+++ config/initializers/full_circle.rb
@@ -3,16 +3,19 @@ ClosingDay = 31
 ClosingMonth = 12
 Date::DATE_FORMATS[:default] = '%d-%m-%Y'
 Money.default_currency = Money::Currency.new('MYR')
-CompanyAddress = Address.new do |t|
-  t.address1 = 'No. 81, Golden Dragon Garden'
-  t.address2 = nil
-  t.address3 = nil
-  t.area     = nil
-  t.city     = 'Kampar'
-  t.zipcode  = '31900' 
-  t.state    = 'Perak'
-  t.country  = 'Malaysia'
-  t.tel_no   = '05-2563277'
-  t.fax_no   = '016-9260462'
-  t.reg_no   = '33704-U'
+
+if Address.table_exists?
+  CompanyAddress = Address.new do |t|
+    t.address1 = 'No. 81, Golden Dragon Garden'
+    t.address2 = nil
+    t.address3 = nil
+    t.area     = nil
+    t.city     = 'Kampar'
+    t.zipcode  = '31900' 
+    t.state    = 'Perak'
+    t.country  = 'Malaysia'
+    t.tel_no   = '05-2563277'
+    t.fax_no   = '016-9260462'
+    t.reg_no   = '33704-U'
+  end
 end
