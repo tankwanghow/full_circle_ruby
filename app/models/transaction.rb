e4407df4ae910dcb79afdb897f28fa277903c8fb
tankwanghow <tankwanghow@gmail.com>
2012-10-07 07:06:46 +0800

    clean death code

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,9 +5,6 @@ class Transaction < ActiveRecord::Base
   validates_numericality_of :debit, :credit
   validates_presence_of :transaction_date, :account, :note, :debit, :credit, :user, :doc
   
-  # include ValidateBelongsTo
-  # validate_belongs_to :account, :name1
-  
   before_destroy :closed?
 
   scope :account, ->(val) { joins(:account).where('accounts.name1 = ?', val) }
@@ -23,14 +20,6 @@ class Transaction < ActiveRecord::Base
     return "C.O.D." if terms == 0
     return "C.B.D." if terms == -1
   end
-
-  # def account_name
-  #   account.name1
-  # end
-
-  # def transaction_amount
-  #   debit > 0 ? debit : credit
-  # end
 
   private
 
