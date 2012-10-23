9255c000f0007a8ef05a09cdc129eeee5a3ec1e1
tankwanghow <tankwanghow@gmail.com>
2012-10-24 00:09:55 +0800

    direct define instance method to comply with rails 4

--- lib/validate_transactions_balance.rb
+++ lib/validate_transactions_balance.rb
@@ -2,9 +2,7 @@ require "active_support/concern"
 
 module ValidateTransactionsBalance
   extend ActiveSupport::Concern
-  module InstanceMethods
-    def validates_transactions_balance
-      raise 'Transactions Not Balance!' if transactions.inject(0) { |sum, p| sum + p.amount } != 0
-    end    
-  end
+  def validates_transactions_balance
+    raise 'Transactions Not Balance!' if transactions.inject(0) { |sum, p| sum + p.amount } != 0
+  end    
 end
