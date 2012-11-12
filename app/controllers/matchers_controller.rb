95ce8769717eac3fadd56c9ad6b303522525bf49
tankwanghow <tankwanghow@gmail.com>
2012-11-13 00:55:48 +0800

    added balance bf

--- app/controllers/matchers_controller.rb
+++ app/controllers/matchers_controller.rb
@@ -1,12 +1,9 @@
 class MatchersController < ApplicationController
   
   def index
+    @balance = Account.find_by_name1(params[:name1]).balance_before(params[:from])
     @matchers = Transaction.with_matched_amount_and_balance(params[:name1], params[:doc_type], params[:doc_id], params[:from], params[:to])
-    if @matchers and @matchers.count > 0
-      render partial: 'matchers/query_field', collection: @matchers
-    else
-      render text: '<h3>No record!</h3>'
-    end
+    render partial: 'matchers/query_field' #, collection: @matchers
   end
   
 end
