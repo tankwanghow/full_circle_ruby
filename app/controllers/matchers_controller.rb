d201fc50feaebfb21bcb0a2b4475d16e43047020
tankwanghow <tankwanghow@gmail.com>
2012-11-26 01:04:53 +0800

    remove query_field params, cleanup

--- app/controllers/matchers_controller.rb
+++ app/controllers/matchers_controller.rb
@@ -3,7 +3,7 @@ class MatchersController < ApplicationController
   def index
     @balance = Account.find_by_name1(params[:name1]).balance_before(params[:from])
     @matchers = Transaction.with_matched_amount_and_balance(params[:name1], params[:doc_type], params[:doc_id], params[:from], params[:to])
-    render partial: 'matchers/query_field' #, collection: @matchers
+    render partial: 'matchers/query_field'
   end
   
 end
--- app/helpers/matchers_helper.rb
+++ app/helpers/matchers_helper.rb
@@ -1,6 +1,6 @@
 module MatchersHelper
 
-  def matcher_attr_name(field, query_field, matcher_id)
+  def matcher_attr_name(field, matcher_id)
     "#{params[:doc_type].underscore}[matchers_attributes][" + matcher_id.to_s + "][#{field.to_s}]"
   end
 
