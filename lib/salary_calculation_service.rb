48f78a4134c770a880895ed8c7997259127641b3
tankwanghow <tankwanghow@gmail.com>
2013-01-09 03:22:00 +0800

    set qty to 0 if unit_price is 0

--- lib/salary_calculation_service.rb
+++ lib/salary_calculation_service.rb
@@ -2,9 +2,13 @@ class SalaryCalculationService
 
   def self.calculate payslip
     @payslip = payslip
-    payslip.salary_notes.select{ |t| !t.marked_for_destruction? and t.amount == 0 }.each do |n|
-      n.quantity = 1
+    @payslip.salary_notes.select{ |t| !t.marked_for_destruction? and t.salary_type.service_method }.each do |n|
       n.unit_price = send(n.salary_type.service_method) if respond_to?(n.salary_type.service_method)
+      if n.unit_price > 0
+        n.quantity = 1
+      else
+        n.quantity = 0
+      end
     end
   end
 
@@ -16,7 +20,7 @@ private
 
   def self.epf_employee
     EpfContributionService.new(@payslip).epf_employee
-  end  
+  end
 
   def self.socso_employer
     SocsoContributionService.new(@payslip).socso_employer
