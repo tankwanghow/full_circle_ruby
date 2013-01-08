0678bcf7ec552edd2d1bda136a98385aa4385722
tankwanghow <tankwanghow@gmail.com>
2013-01-09 03:20:07 +0800

    added warning for repeated monthly payslip

--- app/controllers/pay_slip_generations_controller.rb
+++ app/controllers/pay_slip_generations_controller.rb
@@ -1,9 +1,18 @@
 class PaySlipGenerationsController < ApplicationController
   def new
-    @gen_pay = PaySlipGenerationService.new
+    @pay_slips = []
+    @gen_pay = PaySlipGenerationService.new params[:gen_pay]
   end
 
   def create
-    @pay_slip = PaySlipGenerationService.new params[:gen_pay]
+    emp = Employee.find_by_name(params[:gen_pay][:employee_name])
+    pay_year = params[:gen_pay][:pay_date].to_date.year
+    pay_month = params[:gen_pay][:pay_date].to_date.month
+    @pay_slips = PaySlip.employee(params[:gen_pay][:employee_name]).month_year(pay_month, pay_year)
+    if @pay_slips.size > 0
+      render :new
+    else
+      redirect_to new_pay_slip_path params[:gen_pay]
+    end
   end
 end
