55ec46c721a59f9fddd3995b535a69653920e5f1
tankwanghow <tankwanghow@gmail.com>
2013-02-14 01:21:32 +0800

    search for _report.rb instead of *.rb

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,6 +1,6 @@
 module ReportsHelper
   def all_report_filenames
-    Dir.glob('app/reports/*.rb').map do |t|
+    Dir.glob('app/reports/*_report.rb').map do |t|
       t.chomp('_report.rb').gsub(/app\/reports\//, '')
     end
   end
