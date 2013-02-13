248e6490f75243a0475f87f5ea6d229443873d20
tankwanghow <tankwanghow@gmail.com>
2013-02-13 11:20:12 +0800

    added dossier.yml

--- config/recipes/postgresql.rb
+++ config/recipes/postgresql.rb
@@ -24,12 +24,14 @@ namespace :postgresql do
   task :setup, roles: :app do
     run "mkdir -p #{shared_path}/config"
     template "postgresql.yml.erb", "#{shared_path}/config/database.yml"
+    template "postgresql.yml.erb", "#{shared_path}/config/dossier.yml"
   end
   after "deploy:setup", "postgresql:setup"
 
   desc "Symlink the database.yml file into latest release"
   task :symlink, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/dossier.yml #{release_path}/config/dossier.yml"
   end
   after "deploy:finalize_update", "postgresql:symlink"
 end
