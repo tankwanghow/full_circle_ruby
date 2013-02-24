c7a97620ee11d23a843868a27cd8c1f64e693dcf
tankwanghow <tankwanghow@gmail.com>
2013-02-25 01:40:01 +0800

    reformat spans

--- app/helpers/journals_helper.rb
+++ app/helpers/journals_helper.rb
@@ -1,7 +1,7 @@
 module JournalsHelper
   def render_journal_transaction_fields builder, xies_name
     render 'share/nested_fields', f: builder, xies_name: xies_name, field: 'journals/transaction_fields',
-            headers: [['Account', 'span6'], ['Note', 'span7'], ['Amount', 'span3']],
+            headers: [['Account', 'span6'], ['Note', 'span11'], ['Amount', 'span4']],
             text: 'Add Transaction'
   end
   
