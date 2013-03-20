3a93d7f1a953eca4770ccd7d51759eb1a8f916a1
tankwanghow <tankwanghow@gmail.com>
2013-03-20 11:17:59 +0800

    added check transaction valid

--- lib/validate_transactions_balance.rb
+++ lib/validate_transactions_balance.rb
@@ -3,6 +3,9 @@ require "active_support/concern"
 module ValidateTransactionsBalance
   extend ActiveSupport::Concern
   def validates_transactions_balance
+    transactions.each do |t| 
+      raise 'Transactions generated in invalid' if !t.valid?
+    end 
     raise 'Transactions Not Balance!' if transactions.inject(0) { |sum, p| sum + p.amount.round(2) } != 0
   end    
 end
