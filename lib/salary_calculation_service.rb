772f041625821531834be4c6c2c6e1ccfc884703
tankwanghow <tankwanghow@gmail.com>
2013-05-01 18:36:55 +0800

    bugfixed

--- lib/salary_calculation_service.rb
+++ lib/salary_calculation_service.rb
@@ -2,12 +2,14 @@ class SalaryCalculationService
 
   def self.calculate payslip
     @payslip = payslip
-    @payslip.salary_notes.select{ |t| !t.marked_for_destruction? and t.salary_type.service_method }.each do |n|
-      n.unit_price = send(n.salary_type.service_method) if respond_to?(n.salary_type.service_method)
-      if n.unit_price > 0
-        n.quantity = 1
+    @payslip.salary_notes.select{ |t| !t.marked_for_destruction? and !t.salary_type.service_method.blank? }.each do |n|
+      if respond_to?(n.salary_type.service_method)
+        value = send(n.salary_type.service_method)
+        n.quantity = value[0]
+        n.unit_price = value[1]
       else
-        n.quantity = 0
+        n.quantity = -1
+        n.unit_price = 99999999
       end
     end
   end
@@ -15,23 +17,23 @@ class SalaryCalculationService
 private
 
   def self.epf_employer
-    EpfContributionService.new(@payslip).epf_employer
+    [1, EpfContributionService.new(@payslip).epf_employer]
   end
 
   def self.epf_employee
-    EpfContributionService.new(@payslip).epf_employee
+    [1, EpfContributionService.new(@payslip).epf_employee]
   end
 
   def self.socso_employer
-    SocsoContributionService.new(@payslip).socso_employer
+    [1, SocsoContributionService.new(@payslip).socso_employer]
   end
 
   def self.socso_employee
-    SocsoContributionService.new(@payslip).socso_employee
+    [1, SocsoContributionService.new(@payslip).socso_employee]
   end
 
   def self.pcb_employee
-    PcbCalculationService.new(@payslip).pcb_current_month
+    [1, PcbCalculationService.new(@payslip).pcb_current_month]
   end
 
 end
