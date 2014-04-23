a89c0c62d019f271d4b66957c1bc981793f93f9e
tankwanghow <tankwanghow@gmail.com>
2014-04-24 00:20:58 +0800

    bugfixed

--- app/controllers/harvesting_reports_controller.rb
+++ app/controllers/harvesting_reports_controller.rb
@@ -38,18 +38,21 @@ private
       (select sum(hsd1.harvest_1 + hsd1.harvest_2)*30.0
           from harvesting_slip_details hsd1 inner join harvesting_slips hs1
             on hs1.id = hsd1.harvesting_slip_id
-           and hsd1.flock_id = f.id and hsd1.house_id = h.id
+           and hsd1.flock_id = f.id 
+           and hsd1.house_id = h.id
          where hs1.harvest_date = '#{date.to_s(:db)}')/
        ((select sum(mv1.quantity) 
           from flocks f1 inner join movements mv1
             on mv1.flock_id = f1.id
          inner join houses h1 on h1.id = mv1.house_id
            and mv1.move_date <= '#{date.to_s(:db)}'
+           and f1.id = f.id
            and h1.id = h.id) -
        (select sum(death)
           from harvesting_slip_details hsd1 inner join harvesting_slips hs1
             on hs1.id = hsd1.harvesting_slip_id
-           and hsd1.flock_id = f.id and hsd1.house_id = h.id
+           and hsd1.flock_id = f.id 
+           and hsd1.house_id = h.id
          where hs1.harvest_date <= '#{date.to_s(:db)}'))
     SQL
   end
