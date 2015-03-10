83c202e7134ac7f9dca3a6c3da07e18d19c70ce3
tankwanghow <tankwanghow@gmail.com>
2015-03-10 12:53:51 +0800

    trying to restart git flow

--- app/controllers/loading_orders_controller.rb
+++
@@ -1,2 +0,0 @@
-class LoadingOrdersController < ApplicationController
-end
--- app/models/loading_order.rb
+++
@@ -1,3 +0,0 @@
-class LoadingOrder < ActiveRecord::Base
-  attr_accessible :doc_date, :lorry_no, :purchase_order_id, :sales_order_id, :transporter_id
-end
