253891c62177bf6acebc8a2451f9c102b803dee5
tankwanghow <tankwanghow@gmail.com>
2015-06-01 11:19:19 +0800

    make wider matching documents

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -22,7 +22,7 @@ module InvoicesHelper
       content_tag(:div, 'Matched Documents', class: :bold) +
       object.matchers.map do |t|
         total = total + t.amount
-        content_tag :div, class: :span6 do
+        content_tag :div, class: :span7 do
           link_to t.doc_type + docnolize(t.doc_id, ' #') + " " + t.amount.to_money.format, url_for(controller: t.doc_type.pluralize.underscore, action: :edit, id: t.doc_id)
         end
       end.join(' ').html_safe +
