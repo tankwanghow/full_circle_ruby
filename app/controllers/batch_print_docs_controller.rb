5b91cf8beb00669252f98c301b4f5d3dd835debd
tankwanghow <tankwanghow@gmail.com>
2015-06-06 18:37:33 +0800

    added print route

--- app/controllers/batch_print_docs_controller.rb
+++ app/controllers/batch_print_docs_controller.rb
@@ -1,6 +1,12 @@
 class BatchPrintDocsController < ApplicationController
   def index
-    params[:batch_print_search] ||= {}
-    @docs = Document.searchable_by(params[:batch_print_search]).page(params[:page]).per(25).order('updated_at desc')
+    store_param :batch_print_search
+    @docs = Document.searchable_by(session[:batch_print_search]).page(params[:page]).per(10).order('updated_at desc')
   end
+
+  def print
+    print_docs = params[:doc_ids].select { |k, v| v.to_i == 1 }.map { |k,v| k.to_i }
+
+  end
+
 end  
