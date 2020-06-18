808fbaa0990328d83b8b50a48671c1cdb41f8538
Tan Kwang How <tankwanghow@pop-os.localdomain>
2020-06-18 13:55:32 +0800

    trying cap3

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "full_circle"
+set :repo_url, "git@github.com:tankwanghow/full_circle.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,9 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server "139.162.39.50",
+       :user => "deploy",
+       :roles => %w(app backup cron db web)
 
 # role-based syntax
 # ==================
