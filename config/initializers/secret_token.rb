0003efbd71d16375e8eeec6ca8181055ae6e0ee1
Tan Kwang How <tankwanghow@pop-os.localdomain>
2020-06-20 00:44:03 +0800

    cleanup ignore files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FullCircle::Application.config.secret_token = '51c3de139dbb4f46377639ec0fe205b9e76af0ef5d65a3ce4de9fad19fe618505444cca29e899d4fdc5d30e4c717f57379fecb7b7070802e957330ab9af0a156'
